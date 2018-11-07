4c019f48e6820a7c95565674831e1a01bf612c44
Charlotte Prevost <chaprevost91@gmail.com>
2018-11-07 15:03:36 -0600

    Chatkit addition by Ashley

--- scrum-poker-json-api/controllers/authController.js
+++ scrum-poker-json-api/controllers/authController.js
@@ -1,6 +1,20 @@
 const express 	= require('express');
 const router 	= express.Router();
 const User 		= require('../Models/userModel.js');
+const ChatKit = require('@pusher/chatkit-server');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+
+/***Static***/
+const instanceLocator = 'v1:us1:1a9bd709-cbaf-4b9d-b48c-c1d84ce94ca3';
+const testToken = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/1a9bd709-cbaf-4b9d-b48c-c1d84ce94ca3/token';
+const key = '706e0a3a-c916-46ec-a12d-531051dd25ba:ZlWxtkrmCs/p0aE6rjjTgk4CGchqmKb9RLMig3CSjfw='
+
+const chatkit = new ChatKit.default({
+	instanceLocator: instanceLocator,
+	key: key
+})
 
 // const bcrypt 	= require('bcrypt');
 
@@ -14,7 +28,7 @@ const User 		= require('../Models/userModel.js');
 router.post('/register', async (req, res, next) => {
 
 	try {
-	// await User.deleteMany();
+		// await User.deleteMany();
 
     // const user = await User.find({username: req.body.username}); // Check if user exists
 
@@ -23,6 +37,12 @@ router.post('/register', async (req, res, next) => {
     	console.log(`---------- .post /register - req.body: ----------\n`, req.body);
 
     	const password = req.body.password;
+    	// const { username } = req.body.username
+    	const createChatUser = await chatkit.createUser({
+						id: req.body.username,
+						name: req.body.username
+					})
+    	console.log(createChatUser);
     	// Hash password
 			// const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
@@ -35,7 +55,7 @@ router.post('/register', async (req, res, next) => {
 	    userDbEntry.password 	= password;
 	    // userDbEntry.password = passwordHash;
 
-	    const createdUser 		= await User.create(userDbEntry);
+	    const createdUser = await User.create(userDbEntry);
 
 	    console.log(`---------- .post /register - createdUser: ----------\n`, createdUser);
 
@@ -47,7 +67,7 @@ router.post('/register', async (req, res, next) => {
     	console.log(`---------- .post /register - req.session: ----------\n`, req.session);
 
 	    res.json({
-	      status: 200,
+	      status: 201,
 	      data: 'login successful'
 	    });
     
@@ -55,7 +75,7 @@ router.post('/register', async (req, res, next) => {
 	    // console.log('Sorry! This username has already been taken :(');
     // }
 	} catch(err){
-	    next(err);
+    next(err);
 	}
 });
 
@@ -66,9 +86,10 @@ router.post('/login', async(req, res, next) => {
 
 	try {
     const user = await User.find({username: req.body.username})
+    const authData = await chatkit.authenticate({ userId: req.query.user_id})
 
     if (user.length !== 0){
-
+			res.status(authData.status).send(authData.body)
 	    req.session.username = req.body.username;
 	    req.session.logged   = true;
 	    req.session.userId 	 = user[0]._id;
